cebffe0a78645cfc29b4688e8903fb0159cdbf31
ELMUROD <elmurodtoxtayev1993>
2021-12-08 20:56:44 +0500

    uztoz

---
+++ src/components/Car.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>mashinalar</h2>
+    <img src="https://xs.uz/upload/post/2021/01/05/c0012028277995387dbf06ad1ca910650106.jpg" alt="">
+  </div>
+  
+  
+</template>
+
+
+<script>
+export default {
+  name:"Car"
+}
+</script>
