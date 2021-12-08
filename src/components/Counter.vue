336d09b470d02c8cbe0cbc09b64d88efb1611c32
ELMUROD <elmurodtoxtayev1993>
2021-12-08 20:56:44 +0500

    uztoz yordam

---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="one">
+    <h1>counter:{{count}}</h1>
+    <button @click="plus" class="btn btn-primary">plus</button>
+    <button @click="minus" class="btn btn-danger">minus</button>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Counter",
+    data(){
+      return{
+        count:0
+      }
+    },
+    methods:{
+      plus(){
+        this.count++
+      },
+      minus(){
+        this.count--
+      }
+    }
+  }
+</script>
+
+<style>
+  .one{
+    border: 2px solid greenyellow;
+    margin: 30px;
+    padding: 15px;
+    border-radius: 20px;
+  }
+
+</style>
