c15119a7a93b298f4169ce15eb3208ac0ff933a2
Doston <dostonsayfulloyev>
2021-12-09 10:20:02 +0500

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-       <Student :todo="message"/>
+    <form class="w-50 m-5">
 
+        <div class="form-group">
+          <label for="">name</label>
+          <input type="text" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="">surname</label>
+          <input type="text" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="">age</label>
+          <input type="text" class="form-control">
+        </div>
+
+        <button class="btn btn-primary">add student</button>
+    </form>
+
+       <Student :todo="message" :student="talabalar"/>
 
   </div>
 </template>
--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,6 +1,21 @@
 <template>
  <div>
        <h1>{{todo}}</h1>
+
+      <table class="table table-bordered">
+          <thead>
+              <tr>
+                  <th>name</th> <th>surname</th>  <th>age</th>
+              </tr>
+          </thead>
+
+        <tbody>
+            <tr v-for="item in student" :key="item">
+                <td>{{item.name}}</td>  <td>{{item.surname}}</td>  <td>{{item.age}}</td>
+            </tr>
+        </tbody>
+      </table>
+
  </div>
 
 </template>
@@ -8,7 +23,7 @@
 <script>
   export default {
     name:"Student",
-    props:['todo']
+    props:['todo','student']
   }
 
 </script>
