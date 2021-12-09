a23e74291f06a2e2f185b698b23abf6dde7d8a41
Doston <dostonsayfulloyev>
2021-12-09 09:48:31 +0500

    add array

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,59 @@
 <template>
   <div id="app">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-   <h1> Salom web dunyosi </h1>
-    <p>  <span>
-      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur exercitationem expedita explicabo iusto non, obcaecati optio perspiciatis
-        porro, ratione, reiciendis repellendus veritatis vitae. Eius expedita magni perferendis porro sed.</span>
-      <span>Debitis dolore, ea laborum necessitatibus nostrum officiis pariatur quidem quis? Consectetur dolore iusto quae tempore. Blanditiis cupiditate dolorum, est inventore neque non perferendis quae. Laborum odio quod repellat rerum totam.</span><span>Ab alias blanditiis earum eius fugit ipsa iusto maiores necessitatibus neque officiis quia quidem, repellendus sint soluta vel! Blanditiis consectetur corporis ducimus est modi nihil placeat porro quod saepe soluta?</span><span>Ad debitis enim hic id illum impedit inventore itaque laborum magnam magni maiores minus molestias neque officiis optio quaerat quod recusandae saepe, totam voluptas? Autem labore maxime nemo omnis quaerat.</span><span>A adipisci animi assumenda cupiditate deserunt eos fugit necessitatibus similique sit, voluptatum? Adipisci aliquid deserunt eligendi explicabo ipsam ipsum, necessitatibus odit voluptate. Corporis error harum possimus ratione, similique tempore vitae?</span><span>Ab ducimus, et fuga incidunt inventore iste magni nam natus repellendus voluptates. Amet animi, consequuntur cupiditate ducimus eius eos eveniet, incidunt laboriosam laborum libero, obcaecati odio reprehenderit similique ullam voluptatum!</span><span>Accusantium alias, asperiores delectus deleniti dolor eveniet ipsa ipsam, iusto nemo nihil, obcaecati sunt voluptates? Accusantium, aliquid aperiam aspernatur consectetur dolore eos laborum tenetur! Aliquam debitis dolore praesentium quisquam totam!</span><span>Accusamus delectus dolore error hic necessitatibus nemo optio ratione repellat. A ab assumenda consequatur, delectus dolorum ducimus et facilis, magni, non nulla officiis quam qui rerum tempora tenetur voluptatibus voluptatum!</span><span>Amet at beatae, corporis doloribus est explicabo facere fugiat hic id ipsa laudantium magni maiores maxime minus modi nam nemo nostrum numquam pariatur qui rem rerum saepe unde ut voluptate.</span><span>Adipisci, consectetur, doloremque ea esse est explicabo fugiat inventore iste laboriosam maiores mollitia nesciunt, non nostrum perspiciatis placeat praesentium provident quae quas quasi rem rerum sequi tempore temporibus totam voluptates?</span>
-    </span>
-    </p>
-    <Car/>
-    <Counter/>
-    <Counter/>
-    <Counter/>
-    <Counter/>
-    <Student/>
-    <Head/>
+
+       <Student :todo="message"/>
+
+
   </div>
 </template>
 
 <script>
-import Head from './components/Head.vue'
-import  Car from './components/Car.vue'
-import Counter from './components/Counter.vue'
-import Student  from "./components/Student.vue"
+
+import Student from "./components/Student";
 
 export default {
   name: 'App',
   components: {
-    Car,
-    Counter,
-    Student,
-    Head
+     Student
+  },
+  data(){
+      return{
+        message:"hello wolrd",
+
+        talabalar:[
+          {
+            name:"ali",
+            surname:"valiyev",
+            age:23
+          },
+          {
+            name:"sherali",
+            surname:"valiyev",
+            age:23
+          },
+          {
+            name:"erali",
+            surname:"valiyev",
+            age:23
+          },
+          {
+            name:"begali",
+            surname:"valiyev",
+            age:23
+          },
+          {
+            name:"joniali",
+            surname:"valiyev",
+            age:23
+          },
+          {
+            name:"erali",
+            surname:"valiyev",
+            age:23
+          }
+        ]
+
+      }
   }
 }
 </script>
--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,17 +1,6 @@
 <template>
  <div>
-   <table class="table table-striped">
-   <thead>
-   <tr>
-     <th>id</th> <th>name</th> <th>surname</th> <th>age</th>
-   </tr>
-   </thead>
-   <tbody>
-   <tr v-for="item in talabalar" :key="item">
-    <td>{{id++}}</td><td>{{item.name}}</td> <td>{{item.surname}}</td> <td>{{item.age}}</td>
-   </tr>
-   </tbody>
-   </table>
+       <h1>{{todo}}</h1>
  </div>
 
 </template>
@@ -19,48 +8,8 @@
 <script>
   export default {
     name:"Student",
-      data(){
-      return{
-        id:-403,
-
-        talabalar:[
-          {
-            name:"ali",
-            surname:"valiyev",
-            age:23
-          },
-          {
-            name:"sherali",
-            surname:"valiyev",
-            age:23
-          },
-          {
-            name:"erali",
-            surname:"valiyev",
-            age:23
-          },
-          {
-            name:"begali",
-            surname:"valiyev",
-            age:23
-          },
-          {
-            name:"joniali",
-            surname:"valiyev",
-            age:23
-          },
-          {
-            name:"erali",
-            surname:"valiyev",
-            age:23
-          }
-        ]
-
-      }
-      }
+    props:['todo']
   }
-
-
 
 </script>
 
